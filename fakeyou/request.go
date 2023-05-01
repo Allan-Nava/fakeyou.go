1850ebbe845a057a454f9e548f59a99dfea9f6a7
Allan Nava <allannava95@gmail.com>
2023-05-01 22:06:19 +0200

    fix request login

--- fakeyou/request.go
+++ fakeyou/request.go
@@ -1,6 +1,6 @@
 package fakeyou
 
 type RequestLogin struct {
-	Username string `json:"username" required:"true" validate:"nonnil,min=1"`
+	Username string `json:"username_or_email" required:"true" validate:"nonnil,min=1"`
 	Password string `json:"password" required:"true" validate:"nonnil,min=4"`
 }
